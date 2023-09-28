9e85b9fe2664b417d8dcce2781db0d33a3492b92
LmeLover <tcalilung@gmail.com>
2023-09-28 22:59:41 +1000

    Arduino Sketches

---
+++ Arduino Sketches/Assessment Sketches/ADXL335_Test/ADXL335_Test.ino
@@ -0,0 +1,43 @@
+const int xpin = A0;                  // x-axis of the accelerometer
+
+const int ypin = A1;                  // y-axis
+
+const int zpin = A2;                  // z-axis (only on 3-axis models)
+
+void setup() {
+
+  // initialize the serial communications:
+
+  Serial.begin(9600);
+
+  // Provide ground and power by using the analog inputs as normal digital pins.
+
+  // This makes it possible to directly connect the breakout board to the
+
+  // Arduino. If you use the normal 5V and GND pins on the Arduino,
+
+  // you can remove these lines.
+}
+
+void loop() {
+  // Read the sensor values:
+  int xValue = analogRead(xpin);
+  int yValue = analogRead(ypin);
+  int zValue = analogRead(zpin);
+
+  // Check if any of the values exceed 200:
+  if (xValue > 3000 || yValue > 3000 || zValue > 3000) {
+    Serial.println("Warning: Excessive movement!");
+  } else {
+    // If the values are within the acceptable range, print them as usual:
+    Serial.print(xValue);
+    Serial.print("\t");
+    Serial.print(yValue);
+    Serial.print("\t");
+    Serial.print(zValue);
+    Serial.println();
+  }
+
+  // Delay before next reading:
+  delay(200);
+}
---
+++ Arduino Sketches/Assessment Sketches/ARD2_2220_Test/ARD2_2220_Test.ino
@@ -0,0 +1,24 @@
+int Led = 13; // define LED Interface
+int buttonpin = A0; // define the tilt switch sensor interfaces
+int val; // define numeric variables val
+
+void setup () {
+pinMode(Led, OUTPUT); // define LED as output interface
+pinMode(buttonpin, INPUT); // define the output interface tilt switch sensor
+Serial.begin(9600);}
+
+void loop () {
+val = digitalRead(buttonpin); // digital interface will be assigned a value of 3 to read val
+Serial.println(val);
+// When the tilt sensor detects a signal when the switch, LED flashes
+if (val == HIGH) {
+digitalWrite(Led, HIGH);
+Serial.println("Excessive Tilt!");
+delay(200);
+}
+else {
+digitalWrite(Led, LOW);
+Serial.println("Balanced");
+delay(200);
+}
+}
