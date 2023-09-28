2a0b6d2e4a5d6152e1158008923fbe6eeb37e79b
Tristan C <tcalilung@gmail.com>
2023-09-28 14:02:28 +0100

    bt.py fix rfcomm

--- Tutorial RaspberryPi/bt.py
+++ Tutorial RaspberryPi/bt.py
@@ -4,7 +4,7 @@ import string
 
 # reading and writing data from and to arduino serially.
 # rfcomm0 -> this could be different
-ser = serial.Serial("/dev/rfcomm1", 9600)
+ser = serial.Serial("/dev/rfcomm0", 9600)
 ser.write(str.encode('Start\r\n'))
 
 while True:
